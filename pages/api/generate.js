ca237da80ce383642add208a554c2eaa8009a310
xxmayankxx <mayank@999designs.in>
2022-02-03 02:54:11 -0500

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,7 +8,11 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const completion = await openai.createCompletion("text-davinci-001", {
     prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
+    temperature: 0.5,
+    max_tokens: 60,
+    top_p: 0.7,
+    frequency_penalty: 0.5,
+    presence_penalty: 0,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
@@ -16,12 +20,5 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  return `Marv is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: ${capitalizedAnimal}\nMarv:`;
 }
