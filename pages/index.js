7d944256a8cd8edb23aa9680b87dc9e86f8a28ae
xxmayankxx <mayank@999designs.in>
2022-02-03 02:53:07 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import styles from "./index.module.css";
 
 export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
-
+  const [result, setResult] = useState("I have this overwhelming urge to spit on you");
+  const [question, setQuestion] = useState("");
   async function onSubmit(event) {
     event.preventDefault();
     const response = await fetch("/api/generate", {
@@ -18,29 +18,32 @@ export default function Home() {
     const data = await response.json();
     setResult(data.result);
     setAnimalInput("");
+    setQuestion(animalInput);
   }
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Not so friendly neighborhood chatbot</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
+        {/* <img src="/dog.png" className={styles.icon} /> */}
+        <h3>I'm not so friendly neighborhood chatbot. I run on Sarcasm++</h3>
+                <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Ask me something interesting..."
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Ask" />
         </form>
+        {/* <div className={styles.question}>{question}</div> */}
         <div className={styles.result}>{result}</div>
+        <img src="/roman.png" className={styles.roman} />
       </main>
     </div>
   );
